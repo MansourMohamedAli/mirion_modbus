f7ce97ee64b8f6a9f3bbb41849138941cf5d1c67
mmansour <mohamed.mansour@corys.com>
2023-10-21 15:23:16 -0400

    Able to see eachother but not send data

--- mirion_modbus.cpp
+++ mirion_modbus.cpp
@@ -3,7 +3,10 @@
 //
 #include <iostream>
 #include <modbus.h>
+#include <ws2tcpip.h>
+#include <winsock2.h>
 
+#define NB_CONNECTION (10)
 
 int main()
 {
@@ -20,19 +23,56 @@ int main()
 
 	modbus_mapping_t * mb_mapping;
 	mb_mapping = modbus_mapping_new(0, MODBUS_MAX_READ_BITS, 0, MODBUS_MAX_READ_REGISTERS);
-	mb_mapping->tab_input_registers[0] = 500;
+	mb_mapping->tab_input_registers[1] = 500;
 
 
 	//modbus_write_register(ctx, 1, 50);
 
 	//modbus_free(ctx);
 
+	int server_socket{ -1 };
+	server_socket = modbus_tcp_listen(ctx, NB_CONNECTION);
+	if (server_socket == -1)
+	{
+		fprintf(stderr, "Unable to listen TCP connection\n");
+		modbus_free(ctx);
+		return -1;
+	}
+	int fdmax;
+	int rc{};
+	fd_set refset;
+	fd_set rdset;
+	fdmax = server_socket;
+
+
+	socklen_t addrlen;
+	struct sockaddr_in clientaddr;
+	int newfd;
+	uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH];
+	addrlen = sizeof(clientaddr);
+	memset(&clientaddr, 0, sizeof(clientaddr));
+	newfd = accept(server_socket, (struct sockaddr*) &clientaddr, &addrlen);
+	if (newfd == -1)
+	{
+		std::cout << "Server accept() error";
+	}
+
+	modbus_set_socket(ctx, server_socket);
+	rc = modbus_receive(ctx, query);
+
+	if (rc > 0)
+	{
+		modbus_reply(ctx, query, rc, mb_mapping);
+		std::cout << "Reply sent";
+	}
+	else if (rc == -1)
+	{
+		std::cout << "Connection is closed on socket" << server_socket << "\n";
+	}
+
 
 	return 0;
 }
-
-
-
 
 
 
